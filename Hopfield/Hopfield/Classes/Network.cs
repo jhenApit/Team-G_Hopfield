be7209269a9f94467d9f5c223c7a8b710c11b07c
Estrella Abueva <estrella.abueva@cit.edu>
2023-02-09 17:03:15 +0800

    Transfer code for Network.cs

--- Hopfield/Hopfield/Classes/Network.cs
+++ Hopfield/Hopfield/Classes/Network.cs
@@ -6,18 +6,47 @@ using System.Threading.Tasks;
 
 namespace Hopfield.Classes
 {
-    internal class Network
+    public class Network
     {
-        int[] patrn1 = { 1, 0, 1, 0, 1, 0, 1, 0, 1, 0 }, i;
-        int[] wt1 = { 0, 0, 2, -2, -2, -2, 2, 0, 2 };
-        int[] wt2 = { 0, 0, 0, 0, 0, 0, 0, 0, 2 };
-        int[] wt3 = { 2, 0, 0, -2, -2, -2, 2, 0, 2 };
-        int[] wt4 = { 2, 0, -2, 0, 2, 2, -2, 0, 2 };
-        int[] wt5 = { 2, 0, -2, 2, 0, 2, -2, 0, -2 };
-        int[] wt6 = { 2, 0, -2, 2, 2, 0, -2, 0, -2 };
-        int[] wt7 = { 2, 0, 2, -2, -2, -2, 0, 0, 2 };
-        int[] wt8 = { 0, 2, 0, 0, 0, 0, 0, 0, 0 };
-        int[] wt9 = { 2, 0, 2, -2, -2, -2, 2, 0, 0 }; 
+        private Neuron[] nrn = new Neuron[9];
+        public int[] output = new int[9];
+
+        public Network(int[] a, int[] b, int[] c, int[] d, int[] f, int[] j, int[] k, int[] l, int[] m)
+        {
+            nrn[0] = new Neuron(a);
+            nrn[1] = new Neuron(b);
+            nrn[2] = new Neuron(c);
+            nrn[3] = new Neuron(d);
+            nrn[4] = new Neuron(f);
+            nrn[5] = new Neuron(j);
+            nrn[6] = new Neuron(k);
+            nrn[7] = new Neuron(l);
+            nrn[8] = new Neuron(m);
+        }
+
+        public void Activation(int[] patrn)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    Console.WriteLine("\n nrn[" + i + "].weightv[" + j + "] is " + nrn[i].weightv[j]);
+                }
+
+                nrn[i].activation = nrn[i].Act(9, patrn);
+                Console.WriteLine("\nactivation is " + nrn[i].activation);
+                output[i] = Threshold(nrn[i].activation);
+                Console.WriteLine("\noutput value is " + output[i] + "\n");
+            }
+        }
+
+        public int Threshold(int k)
+        {
+            if (k >= 0)
+                return (1);
+            else
+                return (-1);
 
         }
+    }
 }
