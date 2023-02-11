ec972a2c3bffb390a5c5aea898461e5d0901a0f8
Jhen Sarah Apit <jhensarah.apit@cit.edu>
2023-02-11 13:56:23 +0800

    Update UI and Fix Button Colors

--- Hopfield/Hopfield/Form1.Designer.cs
+++ Hopfield/Hopfield/Form1.Designer.cs
@@ -62,9 +62,9 @@
             // 
             this.ni.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.ni.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ni.Location = new System.Drawing.Point(200, 413);
+            this.ni.Location = new System.Drawing.Point(178, 342);
             this.ni.Name = "ni";
-            this.ni.Size = new System.Drawing.Size(573, 47);
+            this.ni.Size = new System.Drawing.Size(451, 47);
             this.ni.TabIndex = 45;
             // 
             // label1
@@ -72,7 +72,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label1.Location = new System.Drawing.Point(213, 69);
+            this.label1.Location = new System.Drawing.Point(121, 24);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(67, 24);
             this.label1.TabIndex = 66;
@@ -83,7 +83,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label2.Location = new System.Drawing.Point(563, 69);
+            this.label2.Location = new System.Drawing.Point(463, 24);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(80, 24);
             this.label2.TabIndex = 67;
@@ -93,9 +93,9 @@
             // 
             this.label3.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label3.Location = new System.Drawing.Point(49, 424);
+            this.label3.Location = new System.Drawing.Point(29, 351);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(149, 25);
+            this.label3.Size = new System.Drawing.Size(143, 25);
             this.label3.TabIndex = 68;
             this.label3.Text = "Neuron Inputs";
             // 
@@ -103,7 +103,7 @@
             // 
             this.label4.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label4.Location = new System.Drawing.Point(103, 479);
+            this.label4.Location = new System.Drawing.Point(29, 412);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(74, 25);
             this.label4.TabIndex = 70;
@@ -113,16 +113,16 @@
             // 
             this.o.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.o.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.o.Location = new System.Drawing.Point(200, 468);
+            this.o.Location = new System.Drawing.Point(178, 403);
             this.o.Name = "o";
-            this.o.Size = new System.Drawing.Size(573, 47);
+            this.o.Size = new System.Drawing.Size(451, 47);
             this.o.TabIndex = 69;
             // 
             // label6
             // 
             this.label6.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label6.Location = new System.Drawing.Point(85, 535);
+            this.label6.Location = new System.Drawing.Point(29, 471);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(111, 25);
             this.label6.TabIndex = 72;
@@ -132,17 +132,16 @@
             // 
             this.t.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.t.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.t.Location = new System.Drawing.Point(200, 523);
+            this.t.Location = new System.Drawing.Point(178, 462);
             this.t.Name = "t";
-            this.t.Size = new System.Drawing.Size(573, 47);
+            this.t.Size = new System.Drawing.Size(451, 47);
             this.t.TabIndex = 71;
-            this.t.Click += new System.EventHandler(this.t_Click);
             // 
             // Calculate
             // 
             this.Calculate.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.Calculate.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.Calculate.Location = new System.Drawing.Point(549, 611);
+            this.Calculate.Location = new System.Drawing.Point(357, 536);
             this.Calculate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Calculate.Name = "Calculate";
             this.Calculate.Size = new System.Drawing.Size(133, 51);
@@ -155,7 +154,7 @@
             // 
             this.Clear.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.Clear.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.Clear.Location = new System.Drawing.Point(688, 611);
+            this.Clear.Location = new System.Drawing.Point(496, 536);
             this.Clear.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Clear.Name = "Clear";
             this.Clear.Size = new System.Drawing.Size(133, 51);
@@ -168,171 +167,171 @@
             // 
             this.I00.BackColor = System.Drawing.Color.WhiteSmoke;
             this.I00.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.I00.Location = new System.Drawing.Point(108, 99);
+            this.I00.Location = new System.Drawing.Point(29, 58);
             this.I00.Name = "I00";
-            this.I00.Size = new System.Drawing.Size(86, 76);
+            this.I00.Size = new System.Drawing.Size(80, 80);
             this.I00.TabIndex = 75;
             this.I00.UseVisualStyleBackColor = false;
-            this.I00.Click += new System.EventHandler(this.I00_Click);
+            this.I00.Click += new System.EventHandler(this.ColorChange_Click);
             // 
             // I01
             // 
             this.I01.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.I01.Location = new System.Drawing.Point(200, 99);
+            this.I01.Location = new System.Drawing.Point(115, 58);
             this.I01.Name = "I01";
-            this.I01.Size = new System.Drawing.Size(86, 76);
+            this.I01.Size = new System.Drawing.Size(80, 80);
             this.I01.TabIndex = 76;
             this.I01.UseVisualStyleBackColor = false;
-            this.I01.Click += new System.EventHandler(this.I01_Click);
+            this.I01.Click += new System.EventHandler(this.ColorChange_Click);
             // 
             // I02
             // 
             this.I02.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.I02.Location = new System.Drawing.Point(292, 99);
+            this.I02.Location = new System.Drawing.Point(201, 58);
             this.I02.Name = "I02";
-            this.I02.Size = new System.Drawing.Size(86, 76);
+            this.I02.Size = new System.Drawing.Size(80, 80);
             this.I02.TabIndex = 77;
             this.I02.UseVisualStyleBackColor = false;
-            this.I02.Click += new System.EventHandler(this.I02_Click);
+            this.I02.Click += new System.EventHandler(this.ColorChange_Click);
             // 
             // I03
             // 
             this.I03.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.I03.Location = new System.Drawing.Point(108, 183);
+            this.I03.Location = new System.Drawing.Point(29, 144);
             this.I03.Name = "I03";
-            this.I03.Size = new System.Drawing.Size(86, 76);
+            this.I03.Size = new System.Drawing.Size(80, 80);
             this.I03.TabIndex = 78;
             this.I03.UseVisualStyleBackColor = false;
-            this.I03.Click += new System.EventHandler(this.I03_Click);
+            this.I03.Click += new System.EventHandler(this.ColorChange_Click);
             // 
             // I04
             // 
             this.I04.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.I04.Location = new System.Drawing.Point(200, 183);
+            this.I04.Location = new System.Drawing.Point(115, 144);
             this.I04.Name = "I04";
-            this.I04.Size = new System.Drawing.Size(86, 76);
+            this.I04.Size = new System.Drawing.Size(80, 80);
             this.I04.TabIndex = 79;
             this.I04.UseVisualStyleBackColor = false;
-            this.I04.Click += new System.EventHandler(this.I04_Click);
+            this.I04.Click += new System.EventHandler(this.ColorChange_Click);
             // 
             // I05
             // 
             this.I05.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.I05.Location = new System.Drawing.Point(292, 183);
+            this.I05.Location = new System.Drawing.Point(201, 144);
             this.I05.Name = "I05";
-            this.I05.Size = new System.Drawing.Size(86, 76);
+            this.I05.Size = new System.Drawing.Size(80, 80);
             this.I05.TabIndex = 80;
             this.I05.UseVisualStyleBackColor = false;
-            this.I05.Click += new System.EventHandler(this.I05_Click);
+            this.I05.Click += new System.EventHandler(this.ColorChange_Click);
             // 
             // I06
             // 
             this.I06.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.I06.Location = new System.Drawing.Point(108, 267);
+            this.I06.Location = new System.Drawing.Point(29, 230);
             this.I06.Name = "I06";
-            this.I06.Size = new System.Drawing.Size(86, 76);
+            this.I06.Size = new System.Drawing.Size(80, 80);
             this.I06.TabIndex = 81;
             this.I06.UseVisualStyleBackColor = false;
-            this.I06.Click += new System.EventHandler(this.I06_Click);
+            this.I06.Click += new System.EventHandler(this.ColorChange_Click);
             // 
             // I07
             // 
             this.I07.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.I07.Location = new System.Drawing.Point(200, 267);
+            this.I07.Location = new System.Drawing.Point(115, 230);
             this.I07.Name = "I07";
-            this.I07.Size = new System.Drawing.Size(86, 76);
+            this.I07.Size = new System.Drawing.Size(80, 80);
             this.I07.TabIndex = 82;
             this.I07.UseVisualStyleBackColor = false;
-            this.I07.Click += new System.EventHandler(this.I07_Click);
+            this.I07.Click += new System.EventHandler(this.ColorChange_Click);
             // 
             // I08
             // 
             this.I08.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.I08.Location = new System.Drawing.Point(292, 267);
+            this.I08.Location = new System.Drawing.Point(201, 230);
             this.I08.Name = "I08";
-            this.I08.Size = new System.Drawing.Size(86, 76);
+            this.I08.Size = new System.Drawing.Size(80, 80);
             this.I08.TabIndex = 83;
             this.I08.UseVisualStyleBackColor = false;
-            this.I08.Click += new System.EventHandler(this.I08_Click);
+            this.I08.Click += new System.EventHandler(this.ColorChange_Click);
             // 
             // O08
             // 
             this.O08.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.O08.Location = new System.Drawing.Point(655, 267);
+            this.O08.Location = new System.Drawing.Point(549, 230);
             this.O08.Name = "O08";
-            this.O08.Size = new System.Drawing.Size(86, 76);
+            this.O08.Size = new System.Drawing.Size(80, 80);
             this.O08.TabIndex = 92;
             this.O08.UseVisualStyleBackColor = false;
             // 
             // O07
             // 
             this.O07.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.O07.Location = new System.Drawing.Point(563, 265);
+            this.O07.Location = new System.Drawing.Point(463, 230);
             this.O07.Name = "O07";
-            this.O07.Size = new System.Drawing.Size(86, 76);
+            this.O07.Size = new System.Drawing.Size(80, 80);
             this.O07.TabIndex = 91;
             this.O07.UseVisualStyleBackColor = false;
             // 
             // O06
             // 
             this.O06.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.O06.Location = new System.Drawing.Point(471, 267);
+            this.O06.Location = new System.Drawing.Point(377, 230);
             this.O06.Name = "O06";
-            this.O06.Size = new System.Drawing.Size(86, 76);
+            this.O06.Size = new System.Drawing.Size(80, 80);
             this.O06.TabIndex = 90;
             this.O06.UseVisualStyleBackColor = false;
             // 
             // O05
             // 
             this.O05.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.O05.Location = new System.Drawing.Point(655, 183);
+            this.O05.Location = new System.Drawing.Point(549, 144);
             this.O05.Name = "O05";
-            this.O05.Size = new System.Drawing.Size(86, 76);
+            this.O05.Size = new System.Drawing.Size(80, 80);
             this.O05.TabIndex = 89;
             this.O05.UseVisualStyleBackColor = false;
             // 
             // O04
             // 
             this.O04.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.O04.Location = new System.Drawing.Point(563, 183);
+            this.O04.Location = new System.Drawing.Point(463, 144);
             this.O04.Name = "O04";
-            this.O04.Size = new System.Drawing.Size(86, 76);
+            this.O04.Size = new System.Drawing.Size(80, 80);
             this.O04.TabIndex = 88;
             this.O04.UseVisualStyleBackColor = false;
             // 
             // O03
             // 
             this.O03.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.O03.Location = new System.Drawing.Point(471, 183);
+            this.O03.Location = new System.Drawing.Point(377, 144);
             this.O03.Name = "O03";
-            this.O03.Size = new System.Drawing.Size(86, 76);
+            this.O03.Size = new System.Drawing.Size(80, 80);
             this.O03.TabIndex = 87;
             this.O03.UseVisualStyleBackColor = false;
             // 
             // O02
             // 
             this.O02.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.O02.Location = new System.Drawing.Point(655, 99);
+            this.O02.Location = new System.Drawing.Point(549, 58);
             this.O02.Name = "O02";
-            this.O02.Size = new System.Drawing.Size(86, 76);
+            this.O02.Size = new System.Drawing.Size(80, 80);
             this.O02.TabIndex = 86;
             this.O02.UseVisualStyleBackColor = false;
             // 
             // O01
             // 
             this.O01.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.O01.Location = new System.Drawing.Point(563, 99);
+            this.O01.Location = new System.Drawing.Point(463, 58);
             this.O01.Name = "O01";
-            this.O01.Size = new System.Drawing.Size(86, 76);
+            this.O01.Size = new System.Drawing.Size(80, 80);
             this.O01.TabIndex = 85;
             this.O01.UseVisualStyleBackColor = false;
             // 
             // O00
             // 
             this.O00.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.O00.Location = new System.Drawing.Point(471, 99);
+            this.O00.Location = new System.Drawing.Point(377, 58);
             this.O00.Name = "O00";
-            this.O00.Size = new System.Drawing.Size(86, 76);
+            this.O00.Size = new System.Drawing.Size(80, 80);
             this.O00.TabIndex = 84;
             this.O00.UseVisualStyleBackColor = false;
             // 
@@ -341,7 +340,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.ClientSize = new System.Drawing.Size(855, 700);
+            this.ClientSize = new System.Drawing.Size(660, 608);
             this.Controls.Add(this.O08);
             this.Controls.Add(this.O07);
             this.Controls.Add(this.O06);
--- Hopfield/Hopfield/Form1.cs
+++ Hopfield/Hopfield/Form1.cs
@@ -1,5 +1,6 @@
 using Hopfield.Classes;
-using System.Diagnostics;
+using System;
+using System.Collections.Generic;
 
 namespace Hopfield
 {
@@ -27,15 +28,15 @@ namespace Hopfield
 
         private void Calculate_Click(object sender, EventArgs e)
         {
-            pattern[0] = I00.BackColor == Color.Maroon ? 1 : -1;
-            pattern[1] = I01.BackColor == Color.Maroon ? 1 : -1;
-            pattern[2] = I02.BackColor == Color.Maroon ? 1 : -1;
-            pattern[3] = I03.BackColor == Color.Maroon ? 1 : -1;
-            pattern[4] = I04.BackColor == Color.Maroon ? 1 : -1;
-            pattern[5] = I05.BackColor == Color.Maroon ? 1 : -1;
-            pattern[6] = I06.BackColor == Color.Maroon ? 1 : -1;
-            pattern[7] = I07.BackColor == Color.Maroon ? 1 : -1;
-            pattern[8] = I08.BackColor == Color.Maroon ? 1 : -1;
+            pattern[0] = I00.BackColor == Color.Black ? 1 : -1;
+            pattern[1] = I01.BackColor == Color.Black ? 1 : -1;
+            pattern[2] = I02.BackColor == Color.Black ? 1 : -1;
+            pattern[3] = I03.BackColor == Color.Black ? 1 : -1;
+            pattern[4] = I04.BackColor == Color.Black ? 1 : -1;
+            pattern[5] = I05.BackColor == Color.Black ? 1 : -1;
+            pattern[6] = I06.BackColor == Color.Black ? 1 : -1;
+            pattern[7] = I07.BackColor == Color.Black ? 1 : -1;
+            pattern[8] = I08.BackColor == Color.Black ? 1 : -1;
 
             //print pattern
             string str = "";
@@ -64,14 +65,13 @@ namespace Hopfield
             {
                 str += network.threshold[i].ToString() + " ";
             }
+            
             t.Text = str;
-
-
 
             Control[] controls = new Control[] { O00, O01, O02, O03, O04, O05, O06, O07, O08 };
             for (int i = 0; i < controls.Length; i++)
             {
-                controls[i].BackColor = (network.threshold[i] == 1) ? Color.Maroon : Color.WhiteSmoke;
+                controls[i].BackColor = (network.threshold[i] == 1) ? Color.Black : Color.WhiteSmoke;
             }
 
         }
@@ -103,54 +103,11 @@ namespace Hopfield
             t.ResetText();
         }
 
-        private void I00_Click(object sender, EventArgs e)
+        private void ColorChange_Click(object sender, EventArgs e)
         {
-            I00.BackColor = (I00.BackColor == Color.WhiteSmoke) ? Color.Maroon : Color.WhiteSmoke;
+            Button btn = (Button)sender;
+            btn.BackColor = (btn.BackColor == Color.WhiteSmoke) ? Color.Black : Color.WhiteSmoke;
         }
-
-        private void I01_Click(object sender, EventArgs e)
-        {
-            I01.BackColor = (I01.BackColor == Color.WhiteSmoke) ? Color.Maroon : Color.WhiteSmoke;
-        }
-
-        private void I02_Click(object sender, EventArgs e)
-        {
-            I02.BackColor = (I02.BackColor == Color.WhiteSmoke) ? Color.Maroon : Color.WhiteSmoke;
-        }
-
-        private void I03_Click(object sender, EventArgs e)
-        {
-            I03.BackColor = (I03.BackColor == Color.WhiteSmoke) ? Color.Maroon : Color.WhiteSmoke;
-        }
-
-        private void I04_Click(object sender, EventArgs e)
-        {
-            I04.BackColor = (I04.BackColor == Color.WhiteSmoke) ? Color.Maroon : Color.WhiteSmoke;
-        }
-
-        private void I05_Click(object sender, EventArgs e)
-        {
-            I05.BackColor = (I05.BackColor == Color.WhiteSmoke) ? Color.Maroon : Color.WhiteSmoke;
-        }
-
-        private void I06_Click(object sender, EventArgs e)
-        {
-            I06.BackColor = (I06.BackColor == Color.WhiteSmoke) ? Color.Maroon : Color.WhiteSmoke;
-        }
-
-        private void I07_Click(object sender, EventArgs e)
-        {
-            I07.BackColor = (I07.BackColor == Color.WhiteSmoke) ? Color.Maroon : Color.WhiteSmoke;
-        }
-
-        private void I08_Click(object sender, EventArgs e)
-        {
-            I08.BackColor = (I08.BackColor == Color.WhiteSmoke) ? Color.Maroon : Color.WhiteSmoke;
-        }
-
-        private void t_Click(object sender, EventArgs e)
-        {
-
-        }
+        
     }
 }
