0aa14e23466f3548dc442a2dc9b90abc32c265f3
Estrella Abueva <estrella.abueva@cit.edu>
2023-02-11 08:45:41 +0800

    Form changes

--- Hopfield/Hopfield/Form1.Designer.cs
+++ Hopfield/Hopfield/Form1.Designer.cs
@@ -46,7 +46,16 @@
             System.Windows.Forms.Button O02;
             System.Windows.Forms.Button O01;
             System.Windows.Forms.Button O00;
-            this.Reply = new System.Windows.Forms.Label();
+            this.ni = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.o = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.t = new System.Windows.Forms.Label();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
             I00 = new System.Windows.Forms.Button();
             I01 = new System.Windows.Forms.Button();
             I02 = new System.Windows.Forms.Button();
@@ -71,7 +80,7 @@
             // 
             I00.BackColor = System.Drawing.SystemColors.ButtonFace;
             I00.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            I00.Location = new System.Drawing.Point(58, 42);
+            I00.Location = new System.Drawing.Point(93, 74);
             I00.Name = "I00";
             I00.Size = new System.Drawing.Size(75, 57);
             I00.TabIndex = 46;
@@ -83,7 +92,7 @@
             // 
             I01.BackColor = System.Drawing.SystemColors.ButtonFace;
             I01.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            I01.Location = new System.Drawing.Point(139, 42);
+            I01.Location = new System.Drawing.Point(174, 74);
             I01.Name = "I01";
             I01.Size = new System.Drawing.Size(75, 57);
             I01.TabIndex = 47;
@@ -95,7 +104,7 @@
             // 
             I02.BackColor = System.Drawing.SystemColors.ButtonFace;
             I02.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            I02.Location = new System.Drawing.Point(220, 42);
+            I02.Location = new System.Drawing.Point(255, 74);
             I02.Name = "I02";
             I02.Size = new System.Drawing.Size(75, 57);
             I02.TabIndex = 48;
@@ -107,7 +116,7 @@
             // 
             I03.BackColor = System.Drawing.SystemColors.ButtonFace;
             I03.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            I03.Location = new System.Drawing.Point(58, 105);
+            I03.Location = new System.Drawing.Point(93, 137);
             I03.Name = "I03";
             I03.Size = new System.Drawing.Size(75, 57);
             I03.TabIndex = 49;
@@ -119,7 +128,7 @@
             // 
             I04.BackColor = System.Drawing.SystemColors.ButtonFace;
             I04.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            I04.Location = new System.Drawing.Point(139, 105);
+            I04.Location = new System.Drawing.Point(174, 137);
             I04.Name = "I04";
             I04.Size = new System.Drawing.Size(75, 57);
             I04.TabIndex = 50;
@@ -131,7 +140,7 @@
             // 
             I05.BackColor = System.Drawing.SystemColors.ButtonFace;
             I05.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            I05.Location = new System.Drawing.Point(220, 105);
+            I05.Location = new System.Drawing.Point(255, 137);
             I05.Name = "I05";
             I05.Size = new System.Drawing.Size(75, 57);
             I05.TabIndex = 51;
@@ -143,7 +152,7 @@
             // 
             I06.BackColor = System.Drawing.SystemColors.ButtonFace;
             I06.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            I06.Location = new System.Drawing.Point(58, 168);
+            I06.Location = new System.Drawing.Point(93, 200);
             I06.Name = "I06";
             I06.Size = new System.Drawing.Size(75, 57);
             I06.TabIndex = 52;
@@ -155,7 +164,7 @@
             // 
             I07.BackColor = System.Drawing.SystemColors.ButtonFace;
             I07.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            I07.Location = new System.Drawing.Point(139, 168);
+            I07.Location = new System.Drawing.Point(174, 200);
             I07.Name = "I07";
             I07.Size = new System.Drawing.Size(75, 57);
             I07.TabIndex = 53;
@@ -167,7 +176,7 @@
             // 
             I08.BackColor = System.Drawing.SystemColors.ButtonFace;
             I08.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            I08.Location = new System.Drawing.Point(220, 168);
+            I08.Location = new System.Drawing.Point(255, 200);
             I08.Name = "I08";
             I08.Size = new System.Drawing.Size(75, 57);
             I08.TabIndex = 54;
@@ -179,7 +188,7 @@
             // 
             O08.BackColor = System.Drawing.SystemColors.ButtonFace;
             O08.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            O08.Location = new System.Drawing.Point(605, 168);
+            O08.Location = new System.Drawing.Point(570, 200);
             O08.Name = "O08";
             O08.Size = new System.Drawing.Size(75, 57);
             O08.TabIndex = 63;
@@ -190,7 +199,7 @@
             // 
             O07.BackColor = System.Drawing.SystemColors.ButtonFace;
             O07.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            O07.Location = new System.Drawing.Point(524, 168);
+            O07.Location = new System.Drawing.Point(489, 200);
             O07.Name = "O07";
             O07.Size = new System.Drawing.Size(75, 57);
             O07.TabIndex = 62;
@@ -201,7 +210,7 @@
             // 
             O06.BackColor = System.Drawing.SystemColors.ButtonFace;
             O06.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            O06.Location = new System.Drawing.Point(443, 168);
+            O06.Location = new System.Drawing.Point(408, 200);
             O06.Name = "O06";
             O06.Size = new System.Drawing.Size(75, 57);
             O06.TabIndex = 61;
@@ -212,7 +221,7 @@
             // 
             O05.BackColor = System.Drawing.SystemColors.ButtonFace;
             O05.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            O05.Location = new System.Drawing.Point(605, 105);
+            O05.Location = new System.Drawing.Point(570, 137);
             O05.Name = "O05";
             O05.Size = new System.Drawing.Size(75, 57);
             O05.TabIndex = 60;
@@ -223,7 +232,7 @@
             // 
             O04.BackColor = System.Drawing.SystemColors.ButtonFace;
             O04.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            O04.Location = new System.Drawing.Point(524, 105);
+            O04.Location = new System.Drawing.Point(489, 137);
             O04.Name = "O04";
             O04.Size = new System.Drawing.Size(75, 57);
             O04.TabIndex = 59;
@@ -234,7 +243,7 @@
             // 
             O03.BackColor = System.Drawing.SystemColors.ButtonFace;
             O03.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            O03.Location = new System.Drawing.Point(443, 105);
+            O03.Location = new System.Drawing.Point(408, 137);
             O03.Name = "O03";
             O03.Size = new System.Drawing.Size(75, 57);
             O03.TabIndex = 58;
@@ -245,7 +254,7 @@
             // 
             O02.BackColor = System.Drawing.SystemColors.ButtonFace;
             O02.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            O02.Location = new System.Drawing.Point(605, 42);
+            O02.Location = new System.Drawing.Point(570, 74);
             O02.Name = "O02";
             O02.Size = new System.Drawing.Size(75, 57);
             O02.TabIndex = 57;
@@ -256,37 +265,141 @@
             // 
             O01.BackColor = System.Drawing.SystemColors.ButtonFace;
             O01.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            O01.Location = new System.Drawing.Point(524, 42);
+            O01.Location = new System.Drawing.Point(489, 74);
             O01.Name = "O01";
             O01.Size = new System.Drawing.Size(75, 57);
             O01.TabIndex = 56;
             O01.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
             O01.UseVisualStyleBackColor = false;
             // 
-            // Reply
-            // 
-            this.Reply.Location = new System.Drawing.Point(58, 278);
-            this.Reply.Name = "Reply";
-            this.Reply.Size = new System.Drawing.Size(622, 36);
-            this.Reply.TabIndex = 45;
-            // 
             // O00
             // 
             O00.BackColor = System.Drawing.SystemColors.ButtonFace;
             O00.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            O00.Location = new System.Drawing.Point(443, 42);
+            O00.Location = new System.Drawing.Point(408, 74);
             O00.Name = "O00";
             O00.Size = new System.Drawing.Size(75, 57);
             O00.TabIndex = 64;
             O00.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
             O00.UseVisualStyleBackColor = false;
             // 
+            // ni
+            // 
+            this.ni.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ni.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ni.Location = new System.Drawing.Point(175, 310);
+            this.ni.Name = "ni";
+            this.ni.Size = new System.Drawing.Size(502, 36);
+            this.ni.TabIndex = 45;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(186, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 19);
+            this.label1.TabIndex = 66;
+            this.label1.Text = "Input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(493, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 19);
+            this.label2.TabIndex = 67;
+            this.label2.Text = "Output";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(43, 318);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 19);
+            this.label3.TabIndex = 68;
+            this.label3.Text = "Neuron Inputs";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(90, 359);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 19);
+            this.label4.TabIndex = 70;
+            this.label4.Text = "Output";
+            // 
+            // o
+            // 
+            this.o.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.o.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.o.Location = new System.Drawing.Point(175, 351);
+            this.o.Name = "o";
+            this.o.Size = new System.Drawing.Size(502, 36);
+            this.o.TabIndex = 69;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Location = new System.Drawing.Point(74, 401);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(97, 19);
+            this.label6.TabIndex = 72;
+            this.label6.Text = "Threshold";
+            // 
+            // t
+            // 
+            this.t.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.t.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.t.Location = new System.Drawing.Point(175, 392);
+            this.t.Name = "t";
+            this.t.Size = new System.Drawing.Size(502, 36);
+            this.t.TabIndex = 71;
+            // 
+            // Calculate
+            // 
+            this.Calculate.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Calculate.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Calculate.Location = new System.Drawing.Point(480, 458);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(116, 38);
+            this.Calculate.TabIndex = 73;
+            this.Calculate.Text = "Calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Clear.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Clear.Location = new System.Drawing.Point(602, 458);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(116, 38);
+            this.Clear.TabIndex = 74;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.ClientSize = new System.Drawing.Size(748, 405);
+            this.ClientSize = new System.Drawing.Size(748, 525);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.t);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.o);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(O00);
             this.Controls.Add(O08);
             this.Controls.Add(O07);
@@ -305,17 +418,27 @@
             this.Controls.Add(I02);
             this.Controls.Add(I01);
             this.Controls.Add(I00);
-            this.Controls.Add(this.Reply);
+            this.Controls.Add(this.ni);
             this.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.Name = "Form1";
-            this.Text = "Hopfield";
+            this.Text = "Team G Hopfield Activity";
             this.TransparencyKey = System.Drawing.Color.Navy;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-        private Label Reply;
+        private Label ni;
         private Button I00;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label o;
+        private Label label6;
+        private Label t;
+        private Button Calculate;
+        private Button Clear;
     }
 }
--- Hopfield/Hopfield/Form1.cs
+++ Hopfield/Hopfield/Form1.cs
@@ -1,10 +1,23 @@
+using Hopfield.Classes;
+
 namespace Hopfield
 {
     public partial class Form1 : Form
     {
+        public Network network;
+        
         public Form1()
         {
             InitializeComponent();
+            network = new Network(new int[] { 1, 1, 1, -1, -1, -1, 1, 1, 1 },
+                                  new int[] { -1, -1, 1, -1, -1, 1, -1, -1, 1 },
+                                  new int[] { 1, 1, 1, -1, -1, 1, 1, 1, 1 },
+                                  new int[] { 1, 1, 1, -1, -1, 1, 1, 1, 1 },
+                                  new int[] { -1, -1, 1, -1, 1, 1, 1, 1, 1 },
+                                  new int[] { 1, 1, 1, 1, -1, -1, 1, 1, 1 },
+                                  new int[] { 1, 1, 1, 1, -1, -1, 1, 1, 1 },
+                                  new int[] { 1, 1, 1, -1, -1, 1, -1, -1, 1 },
+                                  new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1 });
         }
 
         private void I00_Click(object sender, EventArgs e)
@@ -13,6 +26,14 @@ namespace Hopfield
             btn.BackColor = Color.Maroon;
         }
 
-       
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            ni.Text = "diri ang pattern";
+        } 
     }
 }
