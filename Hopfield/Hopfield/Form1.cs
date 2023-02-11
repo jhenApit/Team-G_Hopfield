d82ce6b6aad1381deade07311e95a242de243d7c
Estrella Abueva <estrella.abueva@cit.edu>
2023-02-11 12:43:48 +0800

    Merge

--- Hopfield/Hopfield/Form1.cs
+++ Hopfield/Hopfield/Form1.cs
@@ -1,4 +1,5 @@
 using Hopfield.Classes;
+using System.Diagnostics;
 
 namespace Hopfield
 {
@@ -21,34 +22,12 @@ namespace Hopfield
                                   new int[] { 0, 2, 0, 0, 0, 0, 0, 0, 0 },
                                   new int[] { 2, 0, 2, -2, -2, -2, 2, 0, 0 }
                                   );
-        }
 
-        private void I00_Click(object sender, EventArgs e)
-        {
-            Button btn = (Button)sender;
-            if (btn.BackColor == Color.WhiteSmoke)
-            {
-                btn.BackColor = Color.Maroon;
-            } else
-            {
-                btn.BackColor = Color.WhiteSmoke;
-            }
-
-            btn.Enabled = true;
+ 
         }
 
         private void Calculate_Click(object sender, EventArgs e)
-        {
-            Button I00 = (Button)Controls.Find("I00", true)[0];
-            Button I01 = (Button)Controls.Find("I01", true)[0];
-            Button I02 = (Button)Controls.Find("I02", true)[0];
-            Button I03 = (Button)Controls.Find("I03", true)[0];
-            Button I04 = (Button)Controls.Find("I04", true)[0];
-            Button I05 = (Button)Controls.Find("I05", true)[0];
-            Button I06 = (Button)Controls.Find("I06", true)[0];
-            Button I07 = (Button)Controls.Find("I07", true)[0];
-            Button I08 = (Button)Controls.Find("I08", true)[0];
-
+        { 
             pattern[0] = I00.BackColor == Color.Maroon ? 1 : -1;
             pattern[1] = I01.BackColor == Color.Maroon ? 1 : -1;
             pattern[2] = I02.BackColor == Color.Maroon ? 1 : -1;
@@ -88,16 +67,6 @@ namespace Hopfield
             }
             t.Text = str;
 
-            //output matrix
-            Button O00 = (Button)Controls.Find("O00", true)[0];
-            Button O01 = (Button)Controls.Find("O01", true)[0];
-            Button O02 = (Button)Controls.Find("O02", true)[0];
-            Button O03 = (Button)Controls.Find("O03", true)[0];
-            Button O04 = (Button)Controls.Find("O04", true)[0];
-            Button O05 = (Button)Controls.Find("O05", true)[0];
-            Button O06 = (Button)Controls.Find("O06", true)[0];
-            Button O07 = (Button)Controls.Find("O07", true)[0];
-            Button O08 = (Button)Controls.Find("O08", true)[0];
 
 
             Control[] controls = new Control[] { O00, O01, O02, O03, O04, O05, O06, O07, O08 };
@@ -105,30 +74,84 @@ namespace Hopfield
             {
                 controls[i].BackColor = (network.threshold[i] == 1) ? Color.Maroon : Color.WhiteSmoke;
             }
+
         }
 
         private void Clear_Click(object sender, EventArgs e)
         {
+            I00.BackColor = SystemColors.Control;
+            I01.BackColor = SystemColors.Control;
+            I02.BackColor = SystemColors.Control;
+            I03.BackColor = SystemColors.Control;
+            I04.BackColor = SystemColors.Control;
+            I05.BackColor = SystemColors.Control;
+            I06.BackColor = SystemColors.Control;
+            I07.BackColor = SystemColors.Control;
+            I08.BackColor = SystemColors.Control;
+            
+            O00.BackColor = SystemColors.Control;
+            O01.BackColor = SystemColors.Control;
+            O02.BackColor = SystemColors.Control;
+            O03.BackColor = SystemColors.Control;
+            O04.BackColor = SystemColors.Control;
+            O05.BackColor = SystemColors.Control;
+            O06.BackColor = SystemColors.Control;
+            O07.BackColor = SystemColors.Control;
+            O08.BackColor = SystemColors.Control;
 
+            ni.ResetText();
+            o.ResetText();
+            t.ResetText();
+        }
+
+        private void I00_Click(object sender, EventArgs e)
+        {
+            I00.BackColor = (I00.BackColor == Color.WhiteSmoke) ? Color.Maroon : Color.WhiteSmoke;
+        }
+
+        private void I01_Click(object sender, EventArgs e)
+        {
+            I01.BackColor = (I01.BackColor == Color.WhiteSmoke) ? Color.Maroon : Color.WhiteSmoke;
+        }
+
+        private void I02_Click(object sender, EventArgs e)
+        {
+            I02.BackColor = (I02.BackColor == Color.WhiteSmoke) ? Color.Maroon : Color.WhiteSmoke;
+        }
+
+        private void I03_Click(object sender, EventArgs e)
+        {
+            I03.BackColor = (I03.BackColor == Color.WhiteSmoke) ? Color.Maroon : Color.WhiteSmoke;
+        }
+
+        private void I04_Click(object sender, EventArgs e)
+        {
+            I04.BackColor = (I04.BackColor == Color.WhiteSmoke) ? Color.Maroon : Color.WhiteSmoke;
+        }
+
+        private void I05_Click(object sender, EventArgs e)
+        {
+            I05.BackColor = (I05.BackColor == Color.WhiteSmoke) ? Color.Maroon : Color.WhiteSmoke;
+        }
+
+        private void I06_Click(object sender, EventArgs e)
+        {
+            I06.BackColor = (I06.BackColor == Color.WhiteSmoke) ? Color.Maroon : Color.WhiteSmoke;
+        }
+
+        private void I07_Click(object sender, EventArgs e)
+        {
+            I07.BackColor = (I07.BackColor == Color.WhiteSmoke) ? Color.Maroon : Color.WhiteSmoke;
+        }
+
+        private void I08_Click(object sender, EventArgs e)
+        {
+            I08.BackColor = (I08.BackColor == Color.WhiteSmoke) ? Color.Maroon : Color.WhiteSmoke;
         }
 
         private void t_Click(object sender, EventArgs e)
         {
 
-        }
-
-        private void I01_Click(object sender, EventArgs e)
-        {
-            Button I01 = (Button)sender;
-
-            if (I01.BackColor == Color.WhiteSmoke)
-            {
-                I01.BackColor = Color.Maroon;
-            }
-            else
-            {
-                I01.BackColor = Color.WhiteSmoke;
-            }
         }
     }
 }
